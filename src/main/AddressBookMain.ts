4fcd00aaf99f81a7a972928a9e01ea148e4d9c25
arsath ali <arsatharsha17@gmail.com>
2026-04-24 11:12:46 +0530

    UC2 - Add contact using console input

--- src/main/AddressBookMain.ts
+++ src/main/AddressBookMain.ts
@@ -1,20 +1,30 @@
 import {Contact} from '../model/Contact';
 import { AddressBook } from '../model/AddressBook';
-
+import readline from "readline-sync";
 
 console.log("Welcome to Address Book Program");
 
 const addressBook = new AddressBook();
 
+// Take input from user - by readline-sync
+const firstName = readline.question("Enter First Name: ");
+const lastName = readline.question("Enter Last Name: ");
+const address = readline.question("Enter Address: ");
+const city = readline.question("Enter City: ");
+const state = readline.question("Enter State: ");
+const zip = readline.question("Enter Zip: ");
+const phone = readline.question("Enter Phone: ");
+const email = readline.question("Enter Email: ");
+
 const contact = new Contact(
-    "Arsha",
-    "K",
-    "123 Street",
-    "Chennai",
-    "TN",
-    "600001",
-    "9876543210",
-    "arsha@email.com"
+    firstName,
+    lastName,
+    address,
+    city,
+    state,
+    zip,
+    phone,
+    email
 );
 
 addressBook.addContact(contact);
